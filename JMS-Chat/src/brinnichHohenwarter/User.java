ae4acd489a8254ca987a138d6cb64fd60dcc124e
niklas hohenwarter <info@niklashohenwarter.com>
2014-11-19 19:56:38 +0100

    User und Chat kopplung verbessert

--- JMS-Chat/src/brinnichHohenwarter/User.java
+++ JMS-Chat/src/brinnichHohenwarter/User.java
@@ -7,23 +7,25 @@ public class User extends Thread {
 	private static String chatRoom;
 	
 	private Chat chat;
+	private MOMConnection con;
 	
-	public User() {
-		Chat chat = new Chat(this);
+	public User(String momIP, String username, String chatRooom) {
+		MOMConnection conChat = new MOMConnection(momIP, chatRoom);
+		Chat chat = new Chat(conChat);
 	}
-	
-
 	public static void main(String[] args) {
 		momIP = args[0];
 		username = args[1];
 		chatRoom = args[2];
-		
-		
 	}
 	
 	@Override
 	public void run(){
 		System.out.println(chat.getMessages());
+	}
+	
+	public static void printMessage(String msg){
+		System.out.println(msg);
 	}
 
 }
