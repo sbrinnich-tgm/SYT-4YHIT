ccb8c9861d28f52239e03651c9631af9b24d8d21
niklas hohenwarter <info@niklashohenwarter.com>
2014-11-19 20:39:49 +0100

    Main Kommentar hinzugefügt

--- JMS-Chat/src/brinnichHohenwarter/Main.java
+++ JMS-Chat/src/brinnichHohenwarter/Main.java
@@ -1,9 +1,14 @@
 package brinnichHohenwarter;
 
+/**
+ * Startet den Chat
+ * 
+ * @author niklas hohenwarter
+ * @author selina brinnich
+ * @version 2014-11-19
+ */
 public class Main {
-
 	public static void main(String[] args) {
 		new User(args[0],args[1],args[2]);
 	}
-
 }
