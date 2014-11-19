d83073aaf97ce337c3d2a0819038e8ea563dc4e3
niklas hohenwarter <info@niklashohenwarter.com>
2014-11-19 19:17:43 +0100

    Thread hinzugefügt

---
+++ JMS-Chat/src/brinnichHohenwarter/Chat.java
@@ -0,0 +1,14 @@
+package brinnichHohenwarter;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class Chat implements MessageListener{
+
+	@Override
+	public void onMessage(Message arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- JMS-Chat/src/brinnichHohenwarter/User.java
+++ JMS-Chat/src/brinnichHohenwarter/User.java
@@ -1,12 +1,17 @@
 package brinnichHohenwarter;
 
-public class User {
+public class User extends Thread {
 	
 	private static String username;
 	private static String momIP;
 	
 
 	public static void main(String[] args) {
+		
+	}
+	
+	@Override
+	public void run(){
 		
 	}
 
