d4a1a19ca18ec8a4e7f6294b5748e2b376151dc6
sbrinnich-tgm <sbrinnich@student.tgm.ac.at>
2014-11-21 14:05:58 +0100

    Added method for creating the chat-Frame

--- JMS-Chat/src/brinnichHohenwarter/GUI.java
+++ JMS-Chat/src/brinnichHohenwarter/GUI.java
@@ -1,5 +1,7 @@
 package brinnichHohenwarter;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,28 +15,44 @@ public class GUI implements UserInterface, ActionListener{
 	private JTextField connectIP;
 	private JTextField connectUsername;
 	private JTextField connectChatroom;
+	
 	private JFrame chatFrame;
-	private JFrame mailFrame;
 	private JTextField chatInput;
 	private JTextArea chatMsg;
+	private JButton chatSend;
+	
+	private JFrame mailFrame;
 	private JTextField mailIP;
 	private JTextArea mailMsg;
 
 	public GUI(){
 		init();
+		createChatFrame();
 	}
 	
 	private void init(){
 		chatFrame = new JFrame("Chat");
 		mailFrame = new JFrame("Mail");
 		connectFrame = new JFrame("Connect");
+		
 		this.connectIP = new JTextField();
 		this.connectUsername = new JTextField();
 		this.connectChatroom = new JTextField();
+		
 		this.chatInput = new JTextField();
-		this.mailIP = new JTextField();
 		this.chatMsg = new JTextArea();
+		this.chatSend = new JButton("Senden");
+		
+		this.mailIP = new JTextField();
 		this.mailMsg = new JTextArea();
+		
+		this.chatInput.addActionListener(this);
+		this.chatSend.addActionListener(this);
+		
+		this.chatInput.setMinimumSize(new Dimension(400,28));
+		this.chatInput.setMaximumSize(new Dimension(400,28));
+		this.chatMsg.setMinimumSize(new Dimension(400,380));
+		this.chatMsg.setMaximumSize(new Dimension(400,380));
 	}
 	
 	private void createConnectFrame(){
@@ -42,7 +60,33 @@ public class GUI implements UserInterface, ActionListener{
 	}
 	
 	private void createChatFrame(){
+		chatFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		chatFrame.setSize(400, 400);
+		chatFrame.setResizable(false);
 		
+		JPanel pSend = new JPanel();
+		pSend.setLayout(new BoxLayout(pSend,BoxLayout.LINE_AXIS));
+		pSend.add(this.chatInput);
+		pSend.add(Box.createRigidArea(new Dimension(30,0)));
+		pSend.add(this.chatSend);
+		
+		JPanel p = new JPanel();
+		p.setLayout(new BoxLayout(p,BoxLayout.PAGE_AXIS));
+		p.add(Box.createRigidArea(new Dimension(0,10)));
+		p.add(this.chatMsg);
+		p.add(Box.createRigidArea(new Dimension(0,5)));
+		p.add(pSend);
+		p.add(Box.createRigidArea(new Dimension(0,10)));
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.LINE_AXIS));
+		mainPanel.add(Box.createRigidArea(new Dimension(10,0)));
+		mainPanel.add(p);
+		mainPanel.add(Box.createRigidArea(new Dimension(10,0)));
+		
+		chatFrame.add(mainPanel);
+		
+		chatFrame.setVisible(true);
 	}
 	
 	private void createMailFrame(){
